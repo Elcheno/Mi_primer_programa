ec12490ca2f55da9a81d8e96fb11a57b567cdf57
Elchenoi <r.c.a2312@gmail.com>
2019-04-09 22:43:49 +0200

    He mejorado el sist. haciendolo selectivo a las respuestas...

--- helado.py
+++ helado.py
@@ -1,25 +1,49 @@
-apetece_helado_input = input("¿Te apetece un helado?  (si / no)").upper()
 
-if apetece_helado_input == "SI":
-    apetece_helado = True
-elif apetece_helado_input == "NO":
-    apetece_helado_input = False
+apetece_helado = input("¿Te apetece un helado?  (Si/No):  ").upper()
+if apetece_helado == "SI":
+    dinero = input("¿Tines dinero para el helado?  (Si/No):  ").upper()
+    if dinero == "SI":
+        heladero_cerca = input("¿Esta el heladero cerca?(Si/No):  ").upper()
+        if heladero_cerca == "SI":
+            print("Genial, vamos a por el helado...")
+        elif heladero_cerca == "NO":
+            print("Valla que mala suerte...")
+        else:
+            print("No te he preguntado eso, pero supongo que no...")
+    elif dinero == "NO":
+        dinero_prestado = input("¿Quieres que te deje dinero para el helado?(Si/No):  ").upper()
+        if dinero_prestado == "SI":
+            heladero_cerca = input("¿Esta el heladero cerca?(Si/No):  ").upper()
+            if heladero_cerca == "SI":
+                print("Genial, vamos a por el helado...")
+            elif heladero_cerca == "NO":
+                print("Valla que mala suerte...")
+            else:
+                print("No te he preguntado eso, pero supongo que no...")
+        elif dinero_prestado == "NO":
+            print("Pues nada...")
+        else:
+            print("No te he preguntado eso, pero supongo que no...")
+elif apetece_helado == "NO":
+    print("Pues nada...")
 else:
-    print("Te he preguntado que si querias un helado lo demas no me interesa, ya imagino que no")
-    apetece_helado_input = False
-
-dinero_input = input("¿Tienes dinero para el helado?  (si / no)").upper()
-tia_input = input ("¿Estas con tia?  (si / no)").upper()
-heladero_input = input("¿Esta el heladero cerca?  (si / no)").upper()
+    print("No te he preguntado eso, pero supongo que no...")
 
 
-dinero = dinero_input == "SI"
-tia = tia_input == "SI"
-permitirselo = dinero or tia
-heladero = heladero_input == "SI"
 
-if apetece_helado or permitirselo or heladero:
-    print("¡Pues comete el puto helado de los cojones!")
 
-else:
-    print("Pues nada")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
